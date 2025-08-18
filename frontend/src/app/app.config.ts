0e7f49a48de8f1e93bb702e2dc0f5dad63efcaf7
Wesley Brito <wesleymuniz20@gmail.com>
2025-08-18 02:38:53 -0300

    feat: add Angular Material and ngx-mask dependencies, update styles, and configure toast message component

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth-interceptor';
 
@@ -9,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideEnvironmentNgxMask()
   ]
 };
