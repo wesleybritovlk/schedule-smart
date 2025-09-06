1fa396a9e82dc1ab65eb5cc446472d127335d814
Wesley Brito <wesleymuniz20@gmail.com>
2025-09-06 02:12:11 -0300

    refactor: remove CompanyBluePanel component and integrate its content directly into company-auth page

--- frontend/src/app/pages/company-auth/company-auth.page.ts
+++ frontend/src/app/pages/company-auth/company-auth.page.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { CompanyAuthPanelComponent } from '../../components/company-auth-panel/company-auth-panel.component';
-import { CompanyBluePanelComponent } from '../../components/company-blue-panel/company-blue-panel.component';
 
 @Component({
   selector: 'app-company-auth',
-  imports: [CompanyAuthPanelComponent, CompanyBluePanelComponent],
+  imports: [CompanyAuthPanelComponent],
   templateUrl: './company-auth.page.html',
   styleUrl: './company-auth.page.scss'
 })
